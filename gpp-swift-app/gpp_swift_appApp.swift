b8a7181abc29e5b79e057107841bc8c7a606affc
Douglas Gomes de Paula <douglas.gomes@maps.com.br>
2024-03-07 16:38:51 -0300

    Initial Commit

---
+++ gpp-swift-app/gpp_swift_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  gpp_swift_appApp.swift
+//  gpp-swift-app
+//
+//  Created by Douglas Gomes de Paula on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct gpp_swift_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
