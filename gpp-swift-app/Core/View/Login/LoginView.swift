8281ca961cfdad16c95c05e60fe9097c82ee5256
Douglas Gomes de Paula <douglasgommez@gmail.com>
2024-03-10 18:43:08 -0300

    Login em andamento

--- gpp-swift-app/Core/View/Login/LoginView.swift
+++ gpp-swift-app/Core/View/Login/LoginView.swift
@@ -14,14 +14,66 @@ struct LoginView: View {
         NavigationStack(
             path: $navigation.path
         ){
-            VStack{
-                Button{
-                    navigation.navigate(to: LoginScreen.home)
-                }label: {
-                    Text("Entrar")
+            ZStack(alignment: .bottom){
+                VStack(spacing: 16){
+                    VStack(){
+                        Image("ufu-logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 60)
+                        
+                        Text("PÓS")
+                            .font(.system(size: 32))
+                            .foregroundStyle(.white)
+                            .bold()
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    TextField("E-mail", text: .constant(""))
+                        .textFieldStyle(.roundedBorder)
+                    
+                    PasswordTextField("Senha", text: .constant(""), showPassword: .constant(false))
+                        .textFieldStyle(.roundedBorder)
+                    
+                    Button{
+                        navigation.navigate(to: LoginScreen.home)
+                    }label: {
+                        Text("Entrar")
+                            .buttonBackground()
+                    }
+                    
+                    Button{
+                        navigation.navigate(to: LoginScreen.forgotPassword)
+                    }label: {
+                        Text("Esqueceu a senha?")
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.white)
+                    }
+                    .buttonStyle(.borderless)
+                    .padding()
+                    
+                    Button{
+                        navigation.navigate(to: LoginScreen.signUp)
+                    }label: {
+                        HStack{
+                            Text("Ainda nãon tem conta? ") + Text("Cadastre-se").bold()
+                        }
+                        .foregroundStyle(.white)
+                    }
+                    .buttonStyle(.borderless)
                 }
+                .padding()
+                .padding(.vertical,62)
+                .background(
+                    Rectangle()
+                        .foregroundStyle(Color.backgroundBlue)
+                        .clipShape(
+                            .rect(topLeadingRadius: 50, bottomLeadingRadius: 0, bottomTrailingRadius: 0, topTrailingRadius: 50)
+                        )
+                        .ignoresSafeArea()
+                )
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
             .background(
                 Image("backgroundImage")
                     .resizable()
--- gpp-swift-app/Extensions/ColorExtensions.swift
+++ gpp-swift-app/Extensions/ColorExtensions.swift
@@ -10,4 +10,5 @@ import SwiftUI
 extension Color {
     static let currentBlue = Color(red: 13/255, green: 110/255, blue: 253/255, opacity: 1)
     static let otherBlue = Color(red: 181/255, green: 180/255, blue: 218/255, opacity: 1)
+    static let backgroundBlue = Color(red: 9/255, green: 34/255, blue: 105/255, opacity: 0.80)
 }
