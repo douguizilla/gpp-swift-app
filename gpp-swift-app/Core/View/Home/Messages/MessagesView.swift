476e290be5cfc2540ac41d4a683561d2a014649d
Douglas Gomes de Paula <douglasgommez@gmail.com>
2024-03-10 23:31:44 -0300

    Implementaçao da leitura dos recados na lista

--- gpp-swift-app/Core/View/Home/Messages/MessagesView.swift
+++ gpp-swift-app/Core/View/Home/Messages/MessagesView.swift
@@ -8,30 +8,54 @@
 import SwiftUI
 
 struct MessagesView: View {
+    @State private var messages = Message.sampleList
     var body: some View {
         VStack{
             List{
-                ForEach(0..<7, id: \.self){ _ in
-                    MessageCell()
+                ForEach(messages.indices, id: \.self){ index in
+                    NavigationLink(value: messages[index]) {
+                        MessageCell(messages[index])
+                            .foregroundStyle(Color.primary)
+                            .swipeActions{
+                                Button{
+                                    messages[index].isRead = !messages[index].isRead
+                                }label: {
+                                    Image(
+                                        systemName: messages[index].isRead ? "message.badge.filled.fill" : "message.fill"
+                                    )
+                                }
+                                .tint(messages[index].isRead ? .gray : .blue)
+                            }
+                    }
                 }
             }
         }
         .navigationTitle("Recados")
         .navigationBarTitleDisplayMode(.large)
+        .navigationDestination(for: Message.self) { message in
+            MessageDetailView(message: message)
+        }
     }
     
-    func MessageCell()->some View {
+    func MessageCell(_ message: Message)->some View {
         VStack(alignment: .leading){
-            Text("Recado 1")
-                .font(.headline)
-            
             HStack{
-                Text("Descrição")
-                Spacer()
-                Text("dd/MM/AAAA")
+                Text(message.title)
+                    .font(.headline)
+                    .horizontalAlignment(.leading)
+                
+                Text(message.date)
+                    .font(.subheadline)
+                    
             }
-            .font(.subheadline)
+            
+            Text(message.text)
+                .multilineTextAlignment(.leading)
+                .horizontalAlignment(.leading)
+                .lineLimit(2)
+                .font(.subheadline)
         }
+        .foregroundStyle(message.isRead ? Color.primary.opacity(0.4) : Color.primary)
     }
 }
 
